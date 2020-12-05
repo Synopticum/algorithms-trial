57fd10d8cd256fa649309a882f9ced73cafec06f
Sergey Novikov <synopticum@gmail.com>
2020-12-05 16:22:42 +0300

    Add Fibonacci iterative solution

--- src/pages/Fibonacci/index.tsx
+++ src/pages/Fibonacci/index.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 type Props = {};
 
+// iterative solution
+const fib = (n: number): number => {
+  let prev = null;
+  let current = 0;
+  let next = 1;
+
+  for (let i = 0; i < n; i++) {
+    prev = current;
+    current = next;
+    next = prev + current;
+  }
+
+  return current;
+};
+
 const Fibonacci: React.FC<Props> = () => {
-  return <div>Hello Fibonacci</div>;
+  return <div>Hello Fibonacci {fib(7)}</div>;
 };
 
 export default Fibonacci;
